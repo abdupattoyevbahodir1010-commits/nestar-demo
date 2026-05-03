9efcc19160e62caaebf0bd87948a145d8ea35594
bahodir_dev177 <12345678+abdupattoyevbahodir1010@users.noreply.github.com>
2026-05-03 20:45:23 +0900

    integration header and footer via Layouts

--- pages/property/[productId]/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-const Detail = () => {
-    const router = useRouter();
-    const {productId} = router.query;
-    return <div>PROPERTY Detailo {productId}</div>;
-};
-
-export default Detail;
