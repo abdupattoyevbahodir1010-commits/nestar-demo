aa250be6e9c729f3735a725de6bdf37ce95ad3cf
bahodir_dev177 <12345678+abdupattoyevbahodir1010@users.noreply.github.com>
2026-05-06 19:56:34 +0900

    feat: publish best layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ import { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-      <Stack>
-        <Stack flexDirection={"column"}>
+      
+        <Stack className={"home-page"}>
       <Stack>
           <Stack className='container'>Popular Properties</Stack>
       </Stack>
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
           <Stack className='container'>Top Properties</Stack>
       </Stack>
         </Stack>
-      </Stack>
+      
   );
 };
 
