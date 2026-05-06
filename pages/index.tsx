3db1fa8d34f6c795c8c5c315809cb36c18a87949
bahodir_dev177 <12345678+abdupattoyevbahodir1010@users.noreply.github.com>
2026-05-06 20:23:06 +0900

    feat:publish main layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,23 @@ import { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
       
-        <Stack className={"home-page"}>
+    <Stack className={"home-page"}>
+        <Stack>
+          <Stack className='container'>Trend Properties</Stack>
+      </Stack>
       <Stack>
           <Stack className='container'>Popular Properties</Stack>
       </Stack>
       <Stack>
-          <Stack className='container'>Top Agents</Stack>
+          <Stack className='container'>Advertisment</Stack>
       </Stack>
       <Stack>
           <Stack className='container'>Top Properties</Stack>
       </Stack>
-        </Stack>
+      <Stack>
+          <Stack className='container'>Top Agents</Stack>
+      </Stack>
+   </Stack>
       
   );
 };
